6e93d2365af8795128f778db6ee337b8fc2067ef
Jean <jflorimo@student.42.fr>
2014-08-21 12:06:30 +0200

    update bdd

--- bdd.sql
+++ bdd.sql
@@ -15,3 +15,19 @@ CREATE TABLE IF NOT EXISTS `category` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL DEFAULT 'NULL',
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
