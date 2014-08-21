8612d77a499c1cb9af482cfd8bb268e59e245a41
Jean <jflorimo@student.42.fr>
2014-08-21 11:13:30 +0200

    login/register

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,8 @@
 $route['default_controller'] = "Home";
 
 $route['[Hh][Oo][Mm][Ee]'] = 'Home';
+$route['[Hh][Oo][Mm][Ee]/login'] = 'Home/login';
+$route['[Hh][Oo][Mm][Ee]/register'] = 'Home/register';
 $route['(:any)'] = 'user/createUser/$1'; //access par user exemple devblogs.com/jflorimo
 $route['404_override'] = '';
 
--- application/views/container/header/default.php
+++ application/views/container/header/default.php
@@ -19,7 +19,7 @@
 
 	<h1>Nouveaux éléments de section, article, header, footer, aside, nav</h1>
 
-	<p>Connexion - deconnexion</p>
+	<p><a href="home/login">Connexion</a> - <a href="home/register">Inscription</a></p>
 </header>
 
 <div id="wrapper">
